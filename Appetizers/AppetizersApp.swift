c31a99512b302ce833a4415b32a455f260a23655
prosvetovvv <vp12@yandex.ru>
2021-01-06 14:29:21 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Vitaly Prosvetov on 06.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
